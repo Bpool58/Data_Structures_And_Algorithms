62d9037f5e9c35e6c391623bc6282949749a410d
Branson Poole <bpool58@wgu.edu>
2025-08-29 04:33:13 -0700

    feat: implement stack and queue using linked list nodes. Includes LinkedList methods (append, prepend, insert_after, remove_after) with push/pop for Stack and enqueue/dequeue for Queue.

---
+++ three_7_1_Main.py
@@ -0,0 +1,28 @@
+from three_7_1_Stack import Stack
+from three_7_1_Quene import Queue
+
+# Stack operations
+num_stack = Stack()
+num_stack.push(45)
+num_stack.push(56)
+num_stack.push(11)
+
+# Output stack
+print('Stack after push:', end=' ')
+node = num_stack.list.head
+while node != None:
+   print(node.data, end=' ')
+   node = node.next
+print()
+
+# Pop 11
+popped_item = num_stack.pop()
+print('Popped:', popped_item)
+
+# Output final stack
+print('Stack after pop:', end=' ')
+node = num_stack.list.head
+while node != None:
+   print(node.data, end=' ')
+   node = node.next
+print('\n')
---
+++ three_7_1_Quene.py
@@ -0,0 +1,24 @@
+from three_7_1_Node import Node
+from three_7_1_LinkedList import LinkedList
+
+
+class Queue:
+    def __init__(self):
+        self.list = LinkedList()
+
+    def enqueue(self, new_item):
+        # Create a new node to hold the item
+        new_node = Node(new_item)
+
+        # Insert as list tail (end of queue)
+        self.list.append(new_node)
+
+    def dequeue(self):
+        # Copy data from list's head node (queue's front node)
+        dequeued_item = self.list.head.data
+
+        # Remove list head
+        self.list.remove_after(None)
+
+        # Return the dequeued item
+        return dequeued_item
